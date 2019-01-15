f54a25c8e86f6d1feffaf7b6a1f449db2b3acf0a
Andrew Go <andrewsgo98@gmail.com>
2019-01-15 17:38:01 +0800

    Finished the Second File

--- json-decode.php
+++ json-decode.php
@@ -1,11 +1,21 @@
 <? php
     
-function w3rfunction($balue,$key)
+function w3rfunction($value,$key)
 {
     echo "$key : $value"."<br>";
 }
 
 
 $json = '{
-"Title": "The Cuckoos Calling"
-}'
+"Title": "The Cuckoos Calling",
+"Author": "Robert Galbraith",
+"Detail":
+        {
+        "Publisher": "Little Brown"
+        }
+}';
+
+$jl = json_decode($json,true);
+
+array_walk_recursive($jl,"w3rfunction");
+?>
